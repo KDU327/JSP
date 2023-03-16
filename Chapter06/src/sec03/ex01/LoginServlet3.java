9dca36d90ff5ffa2b41cd50fa4d787b46efb956f
KDU327 <80666228+KDU327@users.noreply.github.com>
2023-03-16 16:06:26 +0900

    Chapter06
    201995010 김동욱

---
+++ Chapter06/src/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,44 @@
+package sec03.ex01;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "ID : " + user_id;
+		data += "<br>";
+		data += "PW : " + user_pw;
+		data += "</body>";
+		data += "</html>";
+		
+		out.println(data);
+	}
+
+}
---
+++ Chapter06/src/sec04/ex01/LoginServlet5.java
@@ -0,0 +1,58 @@
+package sec04.ex01;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Login5")
+public class LoginServlet5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet 메서드 호출");
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost 메서드 호출");
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doHandle 메서드 호출");
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out  = response.getWriter();
+		
+		String user_id   = request.getParameter("user_id");
+		String user_pw   = request.getParameter("user_pw");
+		String user_addr = request.getParameter("user_address");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "ID : "   + user_id;
+		data += "<br>";
+		data += "PW : "   + user_pw;
+		data += "<br>";
+		data += "addr : " + user_addr;
+		data += "</body>";
+		data += "</html>";
+		
+		out.println(data);
+	}
+
+}
